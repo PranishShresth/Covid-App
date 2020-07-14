5750632f133a157fb51d03989d750ec3fb9643b5
Pranish Shrestha <shresthapranish99@gmail.com>
2020-07-14 21:44:38 +1000

    Updates

---
+++ precache-manifest.e3be1e23e3872b1f8c81e0f1215ba4f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166da8fa200fd006a4f75abdb7babcf5",
+    "url": "/Covid-App/index.html"
+  },
+  {
+    "revision": "eee732b0c72a5a5a8dc1",
+    "url": "/Covid-App/static/css/main.7a41448a.chunk.css"
+  },
+  {
+    "revision": "a3e5ef703f94aac60f58",
+    "url": "/Covid-App/static/js/2.811caefa.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/Covid-App/static/js/2.811caefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee732b0c72a5a5a8dc1",
+    "url": "/Covid-App/static/js/main.2ea29e2b.chunk.js"
+  },
+  {
+    "revision": "ab076dddff185314fdf6",
+    "url": "/Covid-App/static/js/runtime-main.f7a90102.js"
+  },
+  {
+    "revision": "7e59da11a6cda070a911ce2077db8ee1",
+    "url": "/Covid-App/static/media/Spinner.7e59da11.gif"
+  }
+]);
